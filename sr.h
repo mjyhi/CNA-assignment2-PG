0102d91c442ac08af37a6f4832e90665aad2d9ec
mjyhi <464791442@qq.com>
2025-05-04 02:56:03 +0930

    Fix sr.h: add emulator.h include for struct definitions

--- sr.h
+++ sr.h
@@ -1,21 +1,15 @@
 #ifndef SR_H
 #define SR_H
 
-// Function prototypes for Selective Repeat protocol
+#define BIDIRECTIONAL 0
 
-// Initializes sender-side variables (A)
+#include "emulator.h"  /* Required for struct msg and struct pkt definitions */
+
+/* Function prototypes for Selective Repeat protocol */
 void A_init(void);
-
-// Initializes receiver-side variables (B)
 void B_init(void);
-
-// Called when the application layer sends a message (A)
 void A_output(struct msg);
-
-// Called when an ACK packet is received at the sender (A)
 void A_input(struct pkt);
-
-// Called when timer expires at the sender (A)
 void A_timerinterrupt(void);
 
 #endif
