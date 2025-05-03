ef2958cc6e4024bf97a3bf1d7690a6163c6116a2
mjyhi <464791442@qq.com>
2025-05-04 02:46:46 +0930

    Add sr.h header with function declarations for Selective Repeat

--- sr.h
+++ sr.h
@@ -1,11 +1,21 @@
-extern void A_init(void);
-extern void B_init(void);
-extern void A_input(struct pkt);
-extern void B_input(struct pkt);
-extern void A_output(struct msg);
-extern void A_timerinterrupt(void);
+#ifndef SR_H
+#define SR_H
 
-/* included for extension to bidirectional communication */
-#define BIDIRECTIONAL 0       /*  0 = A->B  1 =  A<->B */
-extern void B_output(struct msg);
-extern void B_timerinterrupt(void);
+// Function prototypes for Selective Repeat protocol
+
+// Initializes sender-side variables (A)
+void A_init(void);
+
+// Initializes receiver-side variables (B)
+void B_init(void);
+
+// Called when the application layer sends a message (A)
+void A_output(struct msg);
+
+// Called when an ACK packet is received at the sender (A)
+void A_input(struct pkt);
+
+// Called when timer expires at the sender (A)
+void A_timerinterrupt(void);
+
+#endif
